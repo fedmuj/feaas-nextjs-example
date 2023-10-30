8943fdef17d38f8dd6bdedbc7ae1007648cf4d30
Federico Mujica <federicomujica1@gmail.com>
2023-10-30 10:46:56 +0100

    chakra

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,39 @@
-import * as FEAAS from '@sitecore-feaas/clientside/react'
-import dynamic from 'next/dynamic'
+import * as FEAAS from "@sitecore-feaas/clientside/react";
+import dynamic from "next/dynamic";
 // byoc/index lists all BYOC components that will be rendered on clientside
-import ExternalComponentBundle from './byoc'
+import ExternalComponentBundle from "./byoc";
 // Following is BYOC components that will be rendered on server side
-
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
 // Regular react component
-import './byoc/ExampleHybridComponent'
+import "./byoc/ExampleHybridComponent";
 // Server with Client inside
 
-import './byoc/ExampleSwappedComponent'
+import "./byoc/ExampleSwappedComponent";
+
+import HeroBanner2 from "./byoc/HeroBanner2";
 
 // Place somewhere in top level, e.g. layout of the app
-FEAAS.enableNextClientsideComponents(dynamic, ExternalComponentBundle)
+FEAAS.enableNextClientsideComponents(dynamic, ExternalComponentBundle);
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-
-      </head>
-      <body >
-      {/*
+      <head></head>
+      <body>
+        {/*
         Important to include this on the page (preferrably in layout) so
         clientside bundle is properly prepared by Next.
       */}
-      <FEAAS.ExternalComponentBundle />
-      {children}
+        <ChakraProvider>
+          <FEAAS.ExternalComponentBundle />
+          {children}
+        </ChakraProvider>
       </body>
     </html>
-  )
+  );
 }
